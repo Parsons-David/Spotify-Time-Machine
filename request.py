dcf63c5e1e6f3148081b003fb7f19c892eb55d88
David Parsons <david.parsons@rutgers.edu>
2017-12-20 00:02:15 -0500

    Here is where my Idea Died, at the limit 50

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import requests
 import secrets
 
-endpoint = "https://api.spotify.com/v1/me/player/recently-played"
+endpoint = "https://api.spotify.com/v1/me/player/recently-played?limit=5"
 
 header = {
     'Authorization': 'Bearer %s' % secrets.access_token,
@@ -10,4 +10,41 @@ header = {
 
 r=requests.get(endpoint, headers=header);
 
-print(r.json())
+# print(r.json())
+response_data = r.json()
+
+print(response_data.keys())
+
+tracks = response_data.get("items")
+
+n = response_data.get('cursors')
+i = 1
+if tracks:
+    for t in tracks:
+        track = t.get('track')
+        after = t.get('played_at')
+        print(str(i) + ": " + track.get('name'))
+        # print(track.get())
+        i+=1
+print(n)
+# while(i < 200):
+#     endpoint = "https://api.spotify.com/v1/me/player/recently-played?limit=50&after=" + after
+#
+#     header = {
+#         'Authorization': 'Bearer %s' % secrets.access_token,
+#     }
+#
+#
+#     r=requests.get(endpoint, headers=header);
+#
+#     print(r.json())
+#
+#     tracks = r.json().get("items")
+#
+#     if tracks:
+#         for t in tracks:
+#             track = t.get('track')
+#             after = t.get('played_at')
+#             print(str(i) + ": " + track.get('name'))
+#             print(track.get())
+#             i+=1
